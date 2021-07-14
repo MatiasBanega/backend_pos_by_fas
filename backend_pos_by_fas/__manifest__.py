82531dff32766762d8aa406f92edaf739ee61c61
Fasil <63807062+fasilwdr@users.noreply.github.com>
2021-07-14 15:03:47 +0300

    Add files via upload

---
+++ backend_pos_by_fas/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+
+     'name': "Backend POS",
+     'author': "Fasil",
+     'version': '0.1',
+     'sequence': -1,
+     'website': 'https://www.facebook.com/fasilwdr/',
+     'summary': """
+             Designed by Fasil""",
+     'description': """
+             
+         """,
+     'depends': ['base','point_of_sale'],
+     'data': [
+         'views/pos_config.xml',
+         'views/pos_session.xml',
+         'views/pos_order.xml',
+         'views/views.xml',
+         'views/menu_actions.xml',
+     ],
+    'demo': [],
+
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'images': ['static/description/banner.gif'],
+
+}
